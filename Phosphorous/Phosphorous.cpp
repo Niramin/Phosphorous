5e0824e7c45a0cbc32739080111a9ff195ce32a0
Shashwat <shashwatratna@gmail.com>
2024-04-21 09:49:14 +0530

    Add project files.

---
+++ Phosphorous/Phosphorous.cpp
@@ -0,0 +1,12 @@
+﻿// Phosphorous.cpp : Defines the entry point for the application.
+//
+
+#include "Phosphorous.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
